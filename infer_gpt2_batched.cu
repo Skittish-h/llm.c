a0f9f825d049727fd4bd6954f2e15daac723627e
janrueggi <jan.rueggemeier@gmail.com>
2024-12-30 19:08:21 +0100

    b

--- infer_gpt2_batched.cu
+++ infer_gpt2_batched.cu
@@ -149,11 +149,11 @@ int main(int argc, char *argv[]) {
 
     unsigned long long sample_rng_state = (unsigned long long)args.seed;
 
-    printf('a')
+    printf('a');
     for (size_t i = 0; i < 16; i++)
     {
         int t = 1;
-        printf("You entered: %d", i)
+        printf("You entered: %d", i);
         promptloader_next_batch(&loader);
         while (t < T && loader.inputs[t] != eot_token)
         {
@@ -162,7 +162,7 @@ int main(int argc, char *argv[]) {
         }
         t = 1;
     }
-    printf('b')
+    printf('b');
     
     promptloader_next_batch(&loader);
     int t = 1;
