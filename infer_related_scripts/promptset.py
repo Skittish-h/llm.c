b2989822d0044a797216e888863f2931aa68a367
janrueggi <jan.rueggemeier@gmail.com>
2025-01-06 23:38:32 +0100

    clean up

--- infer_related_scripts/promptset.py
+++ infer_related_scripts/promptset.py
@@ -11,11 +11,33 @@ Timing inference trials need to be to the same token length as the input file.
 import argparse
 import os
 import tiktoken
-from data_common import download_file, write_datafile
 
 # -----------------------------------------------------------------------------
 DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), "promptset")
 debug = False
+
+def write_datafile(filename, toks, model_desc="gpt-2"):
+    """
+    Saves token data as a .bin file, for reading in C.
+    - First comes a header with 256 int32s
+    - The tokens follow, each as uint16 (gpt-2) or uint32 (llama)
+    """
+    assert len(toks) < 2**31, "token count too large" # ~2.1B tokens
+    assert model_desc in ["gpt-2", "llama-3"], f"unknown model descriptor {model_desc}"
+    info = HEADERS_INFO[model_desc]
+    # construct the header
+    header = np.zeros(256, dtype=np.int32) # header is always 256 int32 values
+    header[0] = info["magic"]
+    header[1] = info["version"]
+    header[2] = len(toks) # number of tokens after the 256*4 bytes of header
+    # construct the data (numpy array of tokens)
+    toks_np = np.array(toks, dtype=info["token_dtype"])
+    # write to file
+    num_bytes = (256 * 4) + (len(toks) * toks_np.itemsize)
+    print(f"writing {len(toks):,} tokens to {filename} ({num_bytes:,} bytes) in the {model_desc} format")
+    with open(filename, "wb") as f:
+        f.write(header.tobytes())
+        f.write(toks_np.tobytes())
 
 def tokenize(n_tokens: int, input_filename: str):
     enc = tiktoken.get_encoding("gpt2")
